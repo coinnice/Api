64f39087262fc723401ec7e483a034b79a4d703b
coinnice <admin@coinnice.com>
2017-09-26 14:55:20 +0800

    增加python示例

--- rest_python/restpy.py
+++ rest_python/restpy.py
@@ -2,8 +2,8 @@ import rest_python
 
 API_SECRET = "69aebb12-6b47-4858-b5ea-7056561466e4"
 API_KEY = "31c068b8-a61b-40d1-82e8-d6b6720274af"
-HOST = "www.coinnice.com"
-PORT = 80
+HOST = "localhost"
+PORT = 18080
 
 rest = rest_python.RestPythonUtil.RestPythonService(host=HOST, port=PORT, api_key=API_KEY, api_secret=API_SECRET)
 print("获取行情信息", rest.ticker())
